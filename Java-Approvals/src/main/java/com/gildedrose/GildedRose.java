fcf54deccea8c47082fb158060041d04404e44af
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-01 08:03:31 +0100

    lift up conditionals

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -12,8 +12,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE)
-                || item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
 
@@ -32,35 +33,87 @@ class GildedRose {
                         }
                     }
                 } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS)) {
+                            item.quality = item.quality - 1;
+                        }
                     }
                 }
-            }
 
-            if (item.name.equals(SULFURAS)) {
+                if (item.name.equals(SULFURAS)) {
+                } else {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (item.name.equals(AGED_BRIE)) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    } else {
+                        if (item.name.equals(BACKSTAGE_PASSES)) {
+                            item.quality = 0;
+                        } else {
+                            if (item.quality > 0) {
+                                if (!item.name.equals(SULFURAS)) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        }
+                    }
+                }
             } else {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
+                if (item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
+
+                        if (item.name.equals(BACKSTAGE_PASSES)) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
                     }
                 } else {
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        item.quality = 0;
+                    if (item.quality > 0) {
+                        if (!item.name.equals(SULFURAS)) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                }
+
+                if (item.name.equals(SULFURAS)) {
+                } else {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0) {
+                    if (item.name.equals(AGED_BRIE)) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
+                        if (item.name.equals(BACKSTAGE_PASSES)) {
+                            item.quality = 0;
+                        } else {
+                            if (item.quality > 0) {
+                                if (!item.name.equals(SULFURAS)) {
+                                    item.quality = item.quality - 1;
+                                }
                             }
                         }
                     }
                 }
             }
+
         }
     }
 }
