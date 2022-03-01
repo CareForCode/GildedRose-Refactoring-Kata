765041f9cba8c9c3268108f2b600d66424dc3e40
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-01 08:12:39 +0100

    lift up conditionals

---
+++ Java-Approvals/src/main/java/com/gildedrose/Category.java
@@ -0,0 +1,5 @@
+package com.gildedrose;
+
+public interface Category {
+    void handle(Item item);
+}
--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,7 +14,7 @@ class GildedRose {
         for (Item item : items) {
             switch (item.name) {
                 case BACKSTAGE_PASSES:
-                    handleBackstagePasses(item);
+                    new BackstagePassesCategory().handle(item);
                     break;
                 case SULFURAS:
                     handleSulfuras();
@@ -63,27 +63,4 @@ class GildedRose {
         ;
     }
 
-    private void handleBackstagePasses(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        }
-    }
 }
