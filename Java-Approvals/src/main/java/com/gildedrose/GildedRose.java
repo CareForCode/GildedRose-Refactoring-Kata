f6178d870a71a986c36ac526e292ad126331688d
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-01 08:19:24 +0100

    use interface instead of switch

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -23,24 +23,10 @@ class GildedRose {
                     new AgedBrieCategory().handle(item);
                     break;
                 default:
-                    handleOrdinary(item);
+                    new OrdinaryCategory().handle(item);
                     break;
             }
 
-        }
-    }
-
-    private void handleOrdinary(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
         }
     }
 
