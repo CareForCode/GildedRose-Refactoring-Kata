0f44037ca88f609850b739917a2c79798b76cda0
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-01 08:42:48 +0100

    clean up categories

--- Java-Approvals/src/main/java/com/gildedrose/SulfurasCategory.java
+++ Java-Approvals/src/main/java/com/gildedrose/SulfurasCategory.java
@@ -3,7 +3,5 @@ package com.gildedrose;
 public class SulfurasCategory implements Category {
 
     @Override
-    public void handle(Item item) {
-        ;
-    }
+    public void handle(Item item) {}
 }
