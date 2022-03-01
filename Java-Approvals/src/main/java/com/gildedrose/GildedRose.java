3a438eb58d6f98800f383a3252ca2231f96b1cce
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-03-01 08:08:19 +0100

    lift up conditionals

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -13,91 +13,64 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals(BACKSTAGE_PASSES)) {
+                handleBackstagePasses(item);
+            } else if (item.name.equals(SULFURAS)) {
+                handleSulfuras();
+            } else if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
                 }
 
                 item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-            } else if (item.name.equals(SULFURAS)) {
-
-            } else if (item.name.equals(AGED_BRIE)) {
-                if (item.name.equals(AGED_BRIE)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
-
-                    }
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-
-                if (item.name.equals(SULFURAS)) {
-                } else {
-                    item.sellIn = item.sellIn - 1;
-                }
-
-                if (item.sellIn < 0) {
-                    if (item.name.equals(AGED_BRIE)) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
                     }
                 }
             } else {
-                if (item.name.equals(AGED_BRIE)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-
-                    }
-                } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
 
-                if (item.name.equals(SULFURAS)) {
-                } else {
-                    item.sellIn = item.sellIn - 1;
-                }
+                item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
-                    if (item.name.equals(AGED_BRIE)) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
                 }
             }
 
+        }
+    }
+
+    private void handleSulfuras() {
+        ;
+    }
+
+    private void handleBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
     }
 }
