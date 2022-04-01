1eb4ca713fe2bf0d2ccb73f70d256cb7af1113ae
Patrick Scheibert <patrick.scheibert@hotmail.com>
2022-04-01 17:48:17 +0200

    add comment for SulfurasCategory

--- Java-Approvals/src/main/java/com/gildedrose/SulfurasCategory.java
+++ Java-Approvals/src/main/java/com/gildedrose/SulfurasCategory.java
@@ -3,5 +3,7 @@ package com.gildedrose;
 public class SulfurasCategory implements Category {
 
     @Override
-    public void handle(Item item) {}
+    public void handle(Item item) {
+        //do nothing
+    }
 }
